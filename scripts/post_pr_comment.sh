790df0b424981b112a8ebf33dbaf35846fa8ff05
Márcio Eduardo Gomes Barbosa <rgb.marcio@gmail.com>
2023-02-09 18:02:51 -0300

    Add logs

--- scripts/post_pr_comment.sh
+++ scripts/post_pr_comment.sh
@@ -1,5 +1,7 @@
 #/bin/bash
 
+source scripts/utils.sh
+
 export LIGHTHOUSE_URL_REPORT=${lighthouse_link:='https://github.com/olxbr/lighthouse-ci-action'}
 export LIGHTHOUSE_PERFORMANCE=${avg_performance:='-'}
 export LIGHTHOUSE_ACESSIBILITY=${avg_accessibility:='-'}
@@ -16,18 +18,21 @@ export LIGHTHOUSE_TI=${avg_ti:='-'}
 #GH_TOKEN
 
 ## Use teplate and convert
+_log info "Loading template"
 COMMENT=$(envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < templates/pr_comment_template)
 COMMENT="${COMMENT@Q}"
 COMMENT="${COMMENT#\$\'}"
 COMMENT="${COMMENT%\'}"
 
+## Only post if is in a PR
 if [ -n "${PR_NUMBER}" ];
 then
+    _log info "Posting comment"
     curl --location --request POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments" \
         --header "Authorization: token ${GH_TOKEN}" \
         --header "Content-Type: application/json" \
         --data-raw "{\"body\": \"${COMMENT}\"}" \
         --silent -o /dev/null
 else
-    echo "Not commenting on PR :) see full report above"
+    _log info "This may not be a PR so not commenting... See full report above"
 fi
