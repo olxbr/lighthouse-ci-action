bab546f877cf4134b5ece74485d08e226afc6c46
Márcio Eduardo Gomes Barbosa <rgb.marcio@gmail.com>
2023-02-09 16:01:22 -0300

    Add comment on PR

---
+++ scripts/post_pr_comment.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+export LIGHTHOUSE_PERFORMANCE=${avg_performance:='-'}
+export LIGHTHOUSE_ACESSIBILIT=${avg_accessibility:='-'}
+export LIGHTHOUSE_BP=${avg_best_practices:='-'}
+export LIGHTHOUSE_SEO=${avg_seo:='-'}
+export LIGHTHOUSE_PWA=${avg_pwa:='-'}
+export LIGHTHOUSE_FCP=${avg_fcp:='-'}
+export LIGHTHOUSE_SI=${avg_si:='-'}
+export LIGHTHOUSE_LCP=${avg_lcp:='-'}
+export LIGHTHOUSE_TBT=${avg_tbt:='-'}
+export LIGHTHOUSE_CLS=${avg_cls:='-'}
+export LIGHTHOUSE_TI=${avg_ti:='-'}
+#PR_NUMBER
+#GHA_TOKEN
+
+COMMENT=$(envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < templates/pr_comment_template)
+
+#curl --location --request POST 'https://api.github.com/repos/olxbr/${GITHUB_REPOSITORY}/pulls/${PR_NUMBER}/replies' \
+#    --header 'Authorization: token ${GHA_TOKEN}' \
+#    --header 'Content-Type: application/json' \
+#    --data-raw '{"body": "${COMMENT@Q}"}'
+
+echo "${COMMENT@Q}"
