dfcf34a500e6b8773312ff967def007c05c7c30a
Márcio Eduardo Gomes Barbosa <rgb.marcio@gmail.com>
2023-02-10 18:17:05 -0300

    Add substitution of the last comment
    This will help to reduce visual polution in the PR :)

--- scripts/post_pr_comment.sh
+++ scripts/post_pr_comment.sh
@@ -17,9 +17,39 @@ export LIGHTHOUSE_TI=${avg_ti:='-'}
 #PR_NUMBER
 #GH_TOKEN
 
+TEMPLATE="templates/pr_comment_template"
+
+function _check_for_comments () {
+    _log info "Checking for past comments"
+    HEADER=$(cat ${TEMPLATE} | head -n1)
+    COMMENTS=$(curl --location --request GET "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments?per_page=100" \
+            --header "Authorization: token ${GH_TOKEN}" \
+            --silent)
+    LAST_COMMENT_ID=$(jq -c '.[] | select(.body | test("'"^${HEADER}"'")) | select(.user.login == "olxbr-bot") .id' <<< ${COMMENTS} | tail -n1)
+    if [ -n "${LAST_COMMENT_ID}" ];
+    then
+        _log info "Found past comments, deleting... ${LAST_COMMENT_ID}"
+        echo "https://api.github.com/repos/${GITHUB_REPOSITORY}/comments/${LAST_COMMENT_ID}"
+        curl --location --request DELETE "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/comments/${LAST_COMMENT_ID}" \
+            --header "Authorization: token ${GH_TOKEN}" \
+            --silent -o /dev/null || _log warn "Got an error during deletion of ${LAST_COMMENT_ID}! This may be a Token issue!"
+    else
+        _log info "There is no comments of this action in the PR!"
+    fi
+}
+
+function _post_comment () {
+    _log info "Posting comment"
+    curl --location --request POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments" \
+        --header "Authorization: token ${GH_TOKEN}" \
+        --header "Content-Type: application/json" \
+        --data-raw "{\"body\": \"${COMMENT}\"}" \
+        --silent -o /dev/null
+}
+
 ## Use teplate and convert
 _log info "Loading template"
-COMMENT=$(envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < templates/pr_comment_template)
+COMMENT=$(envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < ${TEMPLATE})
 COMMENT="${COMMENT@Q}"
 COMMENT="${COMMENT#\$\'}"
 COMMENT="${COMMENT%\'}"
@@ -27,12 +57,8 @@ COMMENT="${COMMENT%\'}"
 ## Only post if is in a PR
 if [ -n "${PR_NUMBER}" ];
 then
-    _log info "Posting comment"
-    curl --location --request POST "https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PR_NUMBER}/comments" \
-        --header "Authorization: token ${GH_TOKEN}" \
-        --header "Content-Type: application/json" \
-        --data-raw "{\"body\": \"${COMMENT}\"}" \
-        --silent -o /dev/null
+    _check_for_comments
+    #_post_comment
 else
     _log warn "This may not be a PR so not commenting... See full report above"
 fi
