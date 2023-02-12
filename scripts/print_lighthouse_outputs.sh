01dee13d550d6be44f1cf53ed930bf2873b9d379
Jefferson Braga <jeffbraga@gmail.com>
2023-02-11 23:58:24 -0300

    Update print_lighthouse_outputs.sh

--- scripts/print_lighthouse_outputs.sh
+++ scripts/print_lighthouse_outputs.sh
@@ -14,15 +14,15 @@ _log "### Average of ${C_WHT}${RUNS}${C_END} runs ###"
 _log "#########################"
 
 ## Summary (AVG)
-export avg_performance=$(_summary_color $(jq -r '.[].summary.performance'         <<< $JSON | awk "${awk_calc_avg}" || echo '-'))
+export avg_performance=$(jq -r '.[].summary.performance'         <<< $JSON | awk "${awk_calc_avg}" || echo '-')
 #export emoji_performance=$(_summary_emoji ${avg_performance})
-export avg_accessibility=$(_summary_color $(jq -r '.[].summary.accessibility'     <<< $JSON | awk "${awk_calc_avg}" || echo '-'))
+export avg_accessibility=$(jq -r '.[].summary.accessibility'     <<< $JSON | awk "${awk_calc_avg}" || echo '-')
 #export emoji_accessibility=$(_summary_emoji ${avg_accessibility})
-export avg_best_practices=$(_summary_color $(jq -r '.[].summary."best-practices"' <<< $JSON | awk "${awk_calc_avg}" || echo '-'))
+export avg_best_practices=$(jq -r '.[].summary."best-practices"' <<< $JSON | awk "${awk_calc_avg}" || echo '-')
 #export emoji_best_practices=$(_summary_emoji ${avg_best_practices})
-export avg_seo=$(_summary_color $(jq -r '.[].summary.seo'                         <<< $JSON | awk "${awk_calc_avg}" || echo '-'))
+export avg_seo=$(jq -r '.[].summary.seo'                         <<< $JSON | awk "${awk_calc_avg}" || echo '-')
 #export emoji_seo=$(_summary_emoji ${avg_seo})
-export avg_pwa=$(_summary_color $(jq -r '.[].summary.pwa'                         <<< $JSON | awk "${awk_calc_avg}" || echo '-'))
+export avg_pwa=$(jq -r '.[].summary.pwa'                         <<< $JSON | awk "${awk_calc_avg}" || echo '-')
 #export emoji_pwa=$(_summary_emoji ${avg_pwa})
 
 _log "🅢 Summary"
