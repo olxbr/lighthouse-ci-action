e8bbaa3fc9ec02823cd948f7bb8639e60d948939
Jefferson Braga <jeffbraga@gmail.com>
2023-02-11 22:39:17 -0300

    Update print_lighthouse_outputs.sh

--- scripts/print_lighthouse_outputs.sh
+++ scripts/print_lighthouse_outputs.sh
@@ -72,6 +72,7 @@ echo "DEBUG: $(cat ${GITHUB_ENV})"
 ## Print summary to action
 
 TEMPLATE="templates/github_summary_template"
+envsubst < $TEMPLATE
 SUMMARY=$(envsubst "$(printf '${%s} ' $(env | cut -d'=' -f1))" < ${TEMPLATE})
 SUMMARY="${SUMMARY@Q}"
 SUMMARY="${SUMMARY#\$\'}"
