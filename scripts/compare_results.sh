731614d7cac2c499da03d23ba8356d6b3101f53a
Jefferson Braga <jeffbraga@gmail.com>
2023-03-07 20:15:53 -0300

    Update: Forgot to add script :/

---
+++ scripts/compare_results.sh
@@ -0,0 +1,17 @@
+#/bin/bash
+
+## Load common functions
+source scripts/utils.sh
+
+## Declare env variables
+branch_to_compare=${BRANCH_TO_COMPARE}
+
+[[ $branch_to_compare == 'none' ]] && 
+    _log warn "No branches were used to compare. Nothing to do, finished!" &&
+    exit 0
+
+_log "Branch [$branch_to_compare] has been configured to compare results"
+git fetch --force --tags
+git checkout $branch_to_compare
+
+ls -l
