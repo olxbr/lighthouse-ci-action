92ae45aa3c75b6ec8b64eb5488860c23234bb8cc
Jefferson Braga <jeffbraga@gmail.com>
2023-03-11 15:58:33 -0300

    Update: Print Box

--- scripts/main.sh
+++ scripts/main.sh
@@ -153,7 +153,7 @@ echo "aggregateResults=${aggregateResults}" >> "$GITHUB_OUTPUT"
 
 # Compare results recent code with previous (When necessary)
 if [[ "${JSON_COMPARE_RESULTS}" != false ]]; then
-    title_space='\x09\x09\x09\x09\x09\x09\x09\x09'
+    title_space='\x09\x09\x09\x09\x09\x09\x09\x20\x20'
     space_hex='\x20\x20\x20'
     bullet_point_hex="${space_hex}\xe2\x96\xba"
     star_point_hex='\xe2\x9c\xaa'
@@ -173,7 +173,7 @@ if [[ "${JSON_COMPARE_RESULTS}" != false ]]; then
     _log ""
     _log "╔══════════════════════════════════════════════════════╗"
     _log "║${title_space}║"
-    _log "║${space_hex}${space_hex}${space_hex}${space_hex}${C_BLU} RESULT OF THE NEW CODE${C_END} ${space_hex}${space_hex}${space_hex}${space_hex}║"
+    _log "║${space_hex}${space_hex}${space_hex}${space_hex}${space_hex}${C_BLU} RESULT OF THE NEW CODE${C_END}${space_hex}${space_hex}${space_hex}${space_hex}${space_hex}║"
     _log "║${title_space}║"
     _log "╚══════════════════════════════════════════════════════╝"
 
