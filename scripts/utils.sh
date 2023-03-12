8538c923f746ac2d536ba3d6b0db2b1574219fb2
Jefferson Braga <jeffbraga@gmail.com>
2023-03-12 16:06:17 -0300

    Update: debug

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -29,8 +29,11 @@ function _log() {
         msg_total_coll=$2
         msg_last_char=$3
         msg_space_end=$(printf '\\x20%.0s' $(seq 1 $(($msg_length-$msg_total_coll))))
+        echo "ME: ${msg_evaluated}"
+        echo "ML: ${msg_length}"
         echo "T: ${msg_total_coll}"
         echo "C: ${msg_last_char}"
+        echo "msg_space_end: ${msg_space_end}"
         echo "${msg}${msg_space_end}${msg_last_char}"
         msg="${msg}${msg_space_end}${msg_last_char}"
     fi
