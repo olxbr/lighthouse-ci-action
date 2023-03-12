70095b9c7cd84eaf32c2f3943a57eab360452fb2
Jefferson Braga <jeffbraga@gmail.com>
2023-03-12 00:14:13 -0300

    Update: Print Box

--- scripts/main.sh
+++ scripts/main.sh
@@ -186,7 +186,7 @@ if [[ "${JSON_COMPARE_RESULTS}" != false ]]; then
         previous_summary_keys=$(jq -r ".[] | select(.url==\"$previous_url\") | .summary | keys[]" <<< ${previous_results})
         previous_metrics_keys=$(jq -r ".[] | select(.url==\"$previous_url\") | .metrics | keys[]" <<< ${previous_results})
 
-        _log "𝑼𝑹𝑳: $(jq -r ".[$idx].url" <<< ${recent_results})"
+        _log "\x09𝒰𝓇𝓁: $(jq -r ".[$idx].url" <<< ${recent_results})"
         _log "┌──────────────────────────────────────────────────────"
         
         ## for each summary compare to the new version
