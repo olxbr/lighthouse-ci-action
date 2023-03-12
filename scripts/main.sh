29a20699f357ceb6f7b40c82c6f534d5edcf1ad6
Jefferson Braga <jeffbraga@gmail.com>
2023-03-12 20:34:06 -0300

    Update: debug

--- scripts/main.sh
+++ scripts/main.sh
@@ -197,7 +197,7 @@ if [[ "${JSON_COMPARE_RESULTS}" != false ]]; then
         _log "┌$(eval printf '─%.0s' {3..$coll_length})┐"
         
         ## for each summary compare to the new version
-        _log "| \x20\x20🅢 ${C_WHT}Summary (Difference)${C_END}\x09" $(($coll_length+5)) │
+        _log "| \x20\x20🅢 ${C_WHT}Summary (Difference)${C_END}\x09" $(($coll_length)) │
         for s_key in $previous_summary_keys; do
             recent_value=$(jq -r ".[$idx].summary.$s_key" <<< ${recent_results})
             previous_value=$(jq -r ".[] | select(.url==\"$previous_url\") | .summary.$s_key" <<< ${previous_results})
@@ -213,7 +213,7 @@ if [[ "${JSON_COMPARE_RESULTS}" != false ]]; then
         done
 
         ## for each metrics compare to the new version
-        _log "| \x20\x20🅜 ${C_WHT}Metrics (Difference)${C_END}\x09" $(($coll_length+5)) │
+        _log "| \x20\x20🅜 ${C_WHT}Metrics (Difference)${C_END}\x09" $(($coll_length+3)) │
         for m_key in $previous_metrics_keys; do
             recent_value=$(jq -r ".[$idx].metrics.$m_key" <<< ${recent_results})
             previous_value=$(jq -r ".[] | select(.url==\"$previous_url\") | .metrics.$m_key" <<< ${previous_results})
