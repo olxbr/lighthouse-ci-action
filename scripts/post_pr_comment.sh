73783ac50332be07a8a8801a59cd40bd8a2de7cf
Jefferson Braga <jeffbraga@gmail.com>
2023-03-21 08:47:49 -0300

    Update: debug

--- scripts/post_pr_comment.sh
+++ scripts/post_pr_comment.sh
@@ -98,7 +98,7 @@ for url in $urls; do
 
     ## Getting header after variable substitution, escaping the parenthesis
     HEADER=${COMMENT%%\\n*}                      ## Just Title
-    HEADER=$(sed 's,[\(\)],\\\\&,g' <<< $HEADER) ## Escape parenthesis
+    # HEADER=$(sed 's,[\(\)],\\\\&,g' <<< $HEADER) ## Escape parenthesis
 
     ## Only post if is in a PR and github token was filled in
     if [ -n "${PR_NUMBER}" ];
